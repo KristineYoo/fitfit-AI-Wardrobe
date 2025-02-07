31ca7d178c3d79d33ba0cc3ae152a5105c55c75e
Sophia <ss5837@drexel.edu>
2025-02-06 19:33:02 -0500

    feat: created ItemThumbnail component

--- src/Pages/Items.tsx
+++ src/Pages/Items.tsx
@@ -2,16 +2,13 @@ import data from '../../data/WardrobeData.json'
 import ItemThumbnail from '../Components/ItemThumbnail.jsx'
 
 export function Items() {
-  //import JSON file
-  //const data = require('../../data/WardrobeData.json');
-  
   
     return (
       <>
         <h1>Items Page</h1>
         {
-          data.map((data, i) => (
-            <div key={i}>{data.name}</div>
+          data.map((item, i) => (
+            <ItemThumbnail item={item} key={i}/>
           ))
         }
       </>
