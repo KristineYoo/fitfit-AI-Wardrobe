210cd898c66780fddf05d80c584bb34c59da7246
Kristine <kly35@drexel.edu>
2025-02-07 14:44:44 -0500

    feat/ Creating a Log Item Button

--- src/Pages/Items.tsx
+++ src/Pages/Items.tsx
@@ -1,11 +1,17 @@
 import data from '../../data/WardrobeData.json'
 import ItemThumbnail from '../Components/ItemThumbnail.jsx'
+import BasicModal from '../Components/ClothingItemLog.tsx'
+
+
+
+
 
 export function Items() {
   
     return (
       <>
         <h1>Items Page</h1>
+        <BasicModal></BasicModal>
         {
           data.map((item, i) => (
             <ItemThumbnail item={item} key={i}/>
@@ -14,3 +20,4 @@ export function Items() {
       </>
     )
   }
+
