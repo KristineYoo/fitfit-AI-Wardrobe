1036685dae12ff79b5f21f0bfc234f8c756c75fe
Sophia <ss5837@drexel.edu>
2025-02-06 19:05:43 -0500

    feat:imported JSON file to oufits

--- src/Pages/Items.tsx
+++ src/Pages/Items.tsx
@@ -1,4 +1,19 @@
+import data from '../../data/WardrobeData.json'
+import ItemThumbnail from '../Components/ItemThumbnail.jsx'
+
 export function Items() {
+  //import JSON file
+  //const data = require('../../data/WardrobeData.json');
   
-    return <h1>Items Page</h1>
+  
+    return (
+      <>
+        <h1>Items Page</h1>
+        {
+          data.map((data, i) => (
+            <div key={i}>{data.name}</div>
+          ))
+        }
+      </>
+    )
   }
