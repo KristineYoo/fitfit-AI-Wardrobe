ddb4aa2d20d1fe02d59bcb913577904225344f04
Sophia <ss5837@drexel.edu>
2025-02-12 18:12:38 -0500

    feat:added jsonDataTypes, tried to update landing

--- src/Components/FitRecWindow.tsx
+++ src/Components/FitRecWindow.tsx
@@ -1,12 +1,13 @@
 import Paper from '@mui/material/Paper';
 import { FitThumbnail } from './FitThumbnail';
+import { Fit } from '../types/jsonDataTypes';
 
 
-export default function SimplePaper( recs:any ) {
+export default function FitRecWindow( recs:Fit[] ) {
   return (
       <Paper elevation={1} sx={{p:1}}>
         {                                                       //Code not working starting here
-          recs.map((rec:any, i:number) => (
+          recs.map((rec:Fit, i:number) => (
             <FitThumbnail rec={rec} key={"fitThumbnail"+i}/>
           ))                                                    //ending here
         }
--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,21 +1,23 @@
-import data from '../../data/WardrobeData.json'
+import * as data from '../../data/WardrobeData.json'
 import { Container, Typography } from '@mui/material'
 import FitRecWindow from '../components/FitRecWindow'
+import { Fit, Item } from '../types/jsonDataTypes'
 
 export function Landing(){
-    const fit1 = {
-        items: [data[0], data[3], data[4]],
+    const all_items:Item[] = data;
+    const fit1: Fit = {
+        items: [all_items[0], all_items[3], all_items[4]],
         name: "Cute fit",
         tags: ["cute", "pink", "casual"]
     };
 
-    const fit2 = {
-        items: [data[2], data[5]],
+    const fit2: Fit = {
+        items: [all_items[2], all_items[5]],
         name:"Plain and Simple",
         tags:["clean","simple","casual","chill"]
     };
 
-    var fit_recs = [fit1, fit2]
+    var fit_recs:Fit[] = [fit1, fit2]
     
     return (
         <Container sx={{height: "50vh"}}>
