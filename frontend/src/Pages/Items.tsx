cb09a40e269220f1ab0ef1c70c518b828f785de0
Bao Vuong <Bao Vuong>
2025-02-16 14:42:05 -0500

    feat: created ClothingItem interface to follow TypeScript rule, removing the warning error from before

--- frontend/src/Pages/Items.tsx
+++ frontend/src/Pages/Items.tsx
@@ -1,24 +1,38 @@
-import data from '../../data/WardrobeData.json'
+// import data from '../../data/WardrobeData.json'
 import ItemThumbnail from '../components/ItemThumbnail.tsx'
 import LogItemModal from '../components/ClothingItemLog.tsx'
 import Grid from "@mui/material/Grid2";
 import Box from "@mui/material/Box";
+import axios from 'axios';
+import { useEffect } from "react";
+import { useState } from "react";
+import { ClothingItem } from "../types/ClothingItem";
 
 export function Items() {
-  
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+      axios.get("/api/items")
+        .then((res) => {
+          console.log(res.data); // Log for debugging
+          setData(res.data.items || []); // Assuming the response is like { items: [...] }
+        })
+        .catch((err) => console.log(err));
+    }, []);
+    
     return (
       <>
         <h1>Items Page</h1>
         <LogItemModal></LogItemModal>
         <Box sx={{ m: 4 }}>
           <Grid container spacing={3}>
-              {
-                data.map((item, i) => (
-                  <Grid size={2}>
-                    <ItemThumbnail item={item} key={i}/>
+                {
+                data.map((item: ClothingItem) => (
+                  <Grid size={2} key={item.id}>
+                  <ItemThumbnail item={item} />
                   </Grid>
                 ))
-              }
+                }
             </Grid>
         </Box>
       </>
