a3863fb461da1ea7c13d5443b14af6d06012dd07
Kristine <kly35@drexel.edu>
2025-02-19 19:15:30 -0500

    feat: tag components of form

---
+++ src/Components/moodTag.tsx
@@ -0,0 +1,123 @@
+import * as React from 'react';
+import { Theme, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const moodTags = [
+    'Happy',
+    'Relaxed',
+    'Confident',
+    'Chill',
+    'Romantic',
+    'Bold',
+    'Elegant',
+    'Cozy',
+    'Casual',
+    'Edgy',
+    'Playful',
+    'Sophisticated',
+    'Comfortable',
+    'Laid-back',
+    'Flirty',
+    'Minimalistic',
+    'Carefree',
+    'Classy',
+    'Trendy',
+    'Fresh',
+    'Sporty',
+    'Laid-back',
+    'Chic',
+    'Bright',
+    'Subtle',
+    'Vibrant',
+    'Quirky',
+    'Serene',
+    'Rebellious',
+    'Preppy',
+    'Inviting',
+    'Empowered',
+    'Modern',
+    'Vintage',
+    'Timeless',
+    'Adventurous',
+    'Warm',
+    'Optimistic',
+    'Sophisticated',
+    'Fun',
+    'Confident',
+    'Stylish',
+];
+
+
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+    return {
+        fontWeight: personName.includes(name)
+            ? theme.typography.fontWeightMedium
+            : theme.typography.fontWeightRegular,
+    };
+}
+
+export default function MoodTagList() {
+    const theme = useTheme();
+    const [personName, setPersonName] = React.useState<string[]>([]);
+
+    const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+        const {
+            target: { value },
+        } = event;
+        setPersonName(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
+    return (
+        <div>
+            <FormControl sx={{ my: 2, width: 400 }}>
+                <InputLabel id="demo-multiple-chip-label">Select Mood</InputLabel>
+                <Select
+                    labelId="demo-multiple-chip-label"
+                    id="demo-multiple-chip"
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                    renderValue={(selected) => (
+                        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                            {selected.map((value) => (
+                                <Chip key={value} label={value} />
+                            ))}
+                        </Box>
+                    )}
+                    MenuProps={MenuProps}
+                >
+                    {moodTags.map((name) => (
+                        <MenuItem
+                            key={name}
+                            value={name}
+                            style={getStyles(name, personName, theme)}
+                        >
+                            {name}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </div>
+    );
+}
