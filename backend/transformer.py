bf0afa8d0ac65b4a6d770fc68f7f266b98d2dbd0
Sophia <ss5837@drexel.edu>
2025-03-03 20:33:35 -0500

    feat: load model more efficiently

--- backend/transformer.py
+++ backend/transformer.py
@@ -1,8 +1,15 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+sentence_transformer_model = None 
+
+def get_model(): 
+   global sentence_transformer_model 
+   if sentence_transformer_model is None: 
+      sentence_transformer_model = SentenceTransformer('all-MiniLM-L6-v2') 
+   return sentence_transformer_model
 
 def getEmbedding(texts):
+    model = get_model()
     embedding = model.encode(texts)
     return embedding
 
