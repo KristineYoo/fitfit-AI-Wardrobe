ba84487248f9c5c34ed1630a6e65240eef034567
Iain Gore <irg35@drexel.edu>
2025-03-06 23:13:47 -0500

    connected backend to frontend

--- frontend/src/components/FitRecWindow.tsx
+++ frontend/src/components/FitRecWindow.tsx
@@ -6,32 +6,9 @@ import { useEffect, useState } from 'react';
 
 export default function FitRecWindow( {recs} : {recs:Fit[]} ) {
 
-  const [inputValue, setInputValue] = useState('');
-  const [rec, setRecs] = useState([])
-
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-  
-      if (e.key === "Enter") {
-        console.log(inputValue);
-        const data={"text": inputValue}
-        setInputValue('')
-        useEffect(() => {
-          axios.post("/api/recommend", data)
-            .then((res) => {
-              console.log(res.data); // Log for debugging
-              setRecs(res.data.items || []); // Assuming the response is like { items: [...] }
-            })
-            .catch((err) => console.log(err));
-        }, []);
-      }
-
-  }
   
   return (
-      <Paper 
-        elevation={5} 
-        sx={{pl:3, pr:3, pt:1, pb:1, m:2, width:'100%'}}
-      >
+    <Box>
         <Typography 
           variant="h3" 
           sx={{
@@ -54,23 +31,6 @@ export default function FitRecWindow( {recs} : {recs:Fit[]} ) {
             ))
           }
         </Grid2>
-        <Box
-        component="form"
-        sx={{ '& > :not(style)': { m: 5, width: '100ch' , height: '10ch'} }}
-      >
-        <TextField 
-        id="prompt box" 
-        value={inputValue} 
-        onChange={(event) => setInputValue(event.target.value)} 
-        label="Enter Prompt" 
-        variant="outlined" 
-        fullWidth  
-        onKeyUp={handleKeyPress} 
-        multiline
-        color="primary"
-        
-        />
       </Box>
-      </Paper>
   );
 }
