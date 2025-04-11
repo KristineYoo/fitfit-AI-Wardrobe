a6a88f7e4e0a9495dfa660d38da95a254bbef7ba
Iain Gore <irg35@drexel.edu>
2025-04-11 11:20:45 -0400

    Added text boxes and button

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,4 +1,88 @@
+import { Box, Button, Container, TextField } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
+import { useState } from "react";
+
 export function Login() {
+   const [userInputValue, setUserInputValue] = useState('');
+   const [passInputValue, setPassInputValue] = useState('');
   
-    return <h1>Login Page</h1>
+
+   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+     
+         if (e.key === "Enter") {
+           console.log(userInputValue);
+           console.log(passInputValue);
+           const data={"username": userInputValue, "password": passInputValue}
+           setUserInputValue('')
+             axios.post("/api/recommend", data)
+               .then((res) => {
+                 console.log(res.data); // Log for debugging
+   
+               })
+               .catch((err) => console.log(err));
+         }
+   
+     }
+     
+    return (
+       <Container sx={{height: "50vh"}}>
+        <h1>Login</h1>
+        <Paper
+        elevation={5}
+        sx={{ pl: 3, pr: 3, pt: 1, pb: 1, m: 2, width: '100%' }}>
+           <Box
+        component="form"
+        sx={{ '& > :not(style)': { m: 5, width: '100ch' , height: '10ch'} }}
+      >
+        <TextField 
+        id="User Name" 
+        value={userInputValue} 
+        onChange={(event) => setUserInputValue(event.target.value)} 
+        label="User Name" 
+        variant="outlined" 
+        fullWidth  
+        onKeyUp={handleKeyPress} 
+        multiline
+        color="primary"
+        
+        />
+      </Box>
+      <Box
+        component="form"
+        sx={{ '& > :not(style)': { m: 5, width: '100ch' , height: '10ch'} }}
+      >
+        <TextField 
+        id="Password" 
+        value={passInputValue} 
+        onChange={(event) => setPassInputValue(event.target.value)} 
+        label="Password" 
+        variant="outlined" 
+        fullWidth  
+        onKeyUp={handleKeyPress} 
+        multiline
+        color="primary"
+        
+        />
+      </Box>
+        </Paper>
+        <Button 
+        variant="contained"
+        onClick={() => {
+          console.log(userInputValue);
+           console.log(passInputValue);
+           const data={"username": userInputValue, "password": passInputValue}
+           setUserInputValue('')
+             axios.post("/api/recommend", data)
+               .then((res) => {
+                 console.log(res.data); // Log for debugging
+   
+               })
+               .catch((err) => console.log(err));
+        }}
+        >
+          Login
+        </Button>
+        </Container>
+    )
   }
